4f2e807165d33ca0cef1a27e76b8fed0e1576773
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2021-05-10 10:41:29 +0100

    h

--- PHP/Nav.php
+++ PHP/Nav.php
@@ -1,4 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.panel{
+background-color: grey;
+color: white;
+border: 2px striped dark-grey;
+margin-left: 85%;
+padding: 3px;
+
+}
+
+.a hover{
+	color: purple;
+}
+.a visited{
+	color: white;
+}
+</style>
+
+
+</head>
+<body>
+
+<div class="panel">
+<!-- links to the other screens -->
+<!--
+<a href="the php file+location"> this screen</a>
+<a href="the php file+location"> other screen</a>
+-->
+<p> go to this place </p>
+<p> go to the other place </p>
+</div>
+
+
 <?php
+class NavPanel {
 
 ?>
+
+
+
+</body>
+</html>
+
+
+
+
 
