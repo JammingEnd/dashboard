b95dd588b5182b2058c1f0a95ff93eb0a9e1b825
Thiimo <thimosietsma@gmail.com>
2021-05-18 11:37:58 +0200

    api client

--- src/server/curl_helper.php
+++ src/server/curl_helper.php
@@ -1,10 +1,5 @@
 <?php
-
 // This class has all the necessary code for making API calls thru curl library
-
-
-
-
 class CurlHelper {
 
 // This method will perform an action/method thru HTTP/API calls
@@ -12,44 +7,39 @@ class CurlHelper {
 // Method= POST, PUT, GET etc
 // Data= array("param" => "value") ==> index.php?param=value
 
-public static function perform_http_request($method, $url, $data = null)
-{
-    echo "GETTING CALLED!";
-
-    $curl = curl_init($url);
-
-    echo "<br>AFTER CURL INIT!";
-
-    switch ($method)
+    public static function perform_http_request($method, $url, $data = null)
     {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
+        $curl = curl_init($url);
 
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data) {
-                $url = sprintf("%s?%s", $url, http_build_query($data));
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data) {
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+                }
             }
-        }
-    // Optional Authentication:
-    //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+        // Optional Authentication:
+        //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
 
 
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-    $result = curl_exec($curl);
+        $result = curl_exec($curl);
 
-    curl_close($curl);
+        curl_close($curl);
 
-    return $result;
-}
-
+        return $result;
+    }
 }
 ?>
