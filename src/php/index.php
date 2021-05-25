59140fc872fa3a96a373abebff641544821a45c4
Thiimo <thimosietsma@gmail.com>
2021-05-25 08:54:55 +0200

    Added error messages to home page :rocket:

--- src/php/index.php
+++ src/php/index.php
@@ -1,3 +1,14 @@
+<?php
+$padding = 20;
+if (array_key_exists("error1", $_GET)) {
+    $padding -= 5;
+}
+if (array_key_exists("error2", $_GET)) {
+    $padding -= 5;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,15 +21,25 @@
 </head>
 
 <body>
-    <div class="text-center align-middle" style="padding-top: 20%;">
+    <div class="text-center align-middle" style="padding-top: <?php echo $padding ?>%;">
+    <?php 
+    if (array_key_exists("error1", $_GET)) {
+        echo "<div class='alert alert-dismissible alert-danger center-div text-center' style='max-width: 30rem;'>
+        <strong>Er is wat fout gegaan!</strong> Geen kenteken gegeven.</a>
+        </div>";
+    }
+    if (array_key_exists("error2", $_GET)) {
+        echo "<div class='alert alert-dismissible alert-danger center-div text-center' style='max-width: 30rem; margin-top:15px'>
+        <strong>Er is wat fout gegaan!</strong> Kan gegeven kenteken niet vinden.</a>
+        </div>";
+    }
+    ?> 
         <form action="./kentekeninfo.php" method="POST">
             <fieldset>
                 <legend>Kenteken</legend>
             </fieldset>
             <div class="form-group">
-            <!-- TODO make everything uppercase
-                    AND ERROR MESSAGES! -->
-                <input type="text" placeholder="1ABC23" name="kenteken" class="form-label mt-4">
+                <input id="kenteken" type="text" placeholder="1ABC23" name="kenteken" class="form-label mt-4" style="text-transform:uppercase;">
             </div>
             <button type="submit" name="submit" class="btn btn-lg btn-primary">Search</button>
         </form>
