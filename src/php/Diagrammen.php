6b93824616d1b445f6d0f71be20410f5743436df
Thiimo <thimosietsma@gmail.com>
2021-05-26 15:41:13 +0200

    :rocket:

--- src/php/Diagrammen.php
+++ src/php/Diagrammen.php
@@ -5,13 +5,13 @@
 	include_once("../server/types/voertuigen.php");
 
   if (count($_GET) <= 0 || !array_key_exists("kenteken", $_GET) || !array_key_exists("key", $_GET) || !array_key_exists("value", $_GET) || !array_key_exists("merk", $_GET)) {
+    echo "1";
     $host  = $_SERVER['HTTP_HOST'];
     $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
     $extra = 'index.php';
     // header("Location: http://$host$uri/$extra");
     exit; // ???
   }
-
   
   $key = $_GET["key"]; // aantal_cilinders
   $value = $_GET["value"]; // result.
@@ -21,11 +21,16 @@
 
 	$url = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json';
 
+  $urlMerk = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk=" . $merk;
+
 	$results = CurlHelper::getFileContents($url);
 
-	$allVoertuigen = Voertuigen::addVoertuig(json_decode($results), true);
+	$allVoertuigen = Voertuigen::addVoertuig(json_decode($results));
 
-	if (count($allVoertuigen) <= 0) {
+  $resultsMerk = CurlHelper::getFileContents($urlMerk);
+  $merkVoertuigen = Voertuigen::addVoertuig(json_decode($resultsMerk));
+
+	if (count($allVoertuigen) <= 0 || count($merkVoertuigen) <= 0) {
     echo "2";
 		$host  = $_SERVER['HTTP_HOST'];
 		$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
@@ -51,43 +56,48 @@
       break;
   }
 
-  $aantallen = array();
-
-  $valueMap = array();
-
-  foreach ($allVoertuigen as $res) {
-    $result = $res->getPropByName($keyObj);
-    try {
-    if (array_key_exists($result, $valueMap)) {
-      $valueMap[$result] = $valueMap[$result] + 1;
-    } else {
-      $valueMap[$result] = 1;
+  
+  function getValueMap($var1, $var3) {
+    $var2 = array();
+    foreach($var1 as $var) {
+      $result = $var->getPropByName($var3);
+      try {
+      if (array_key_exists($result, $var2)) {
+        $var2[$result] = $var2[$result] + 1;
+      } else {
+        $var2[$result] = 1;
+      }
+      } catch (Exception $e) {
+        echo "<br>";
+        var_dump($res);
+      }
     }
-    } catch (Exception $e) {
-      echo "<br>";
-      var_dump($res);
-    }
+    return $var2;
   }
 
-  $aantallen = array();
-  $lables = array();
+  
 
-  foreach($valueMap as $mapKey => $mapValue) {
-    array_push($aantallen, $mapValue);
-    array_push($lables, ((string) $mapKey) == "" ? "?" : (string)$mapKey);
+
+  $valueMapAll = getValueMap($allVoertuigen, $keyObj);
+  $valueMapMerk = getValueMap($merkVoertuigen, $keyObj);
+
+  $aantallenAll = array();
+  $lablesAll = array();
+
+  
+  $aantallenMerk = array();
+  $lablesMerk = array();
+  
+
+  foreach($valueMapAll as $mapKey => $mapValue) {
+    array_push($aantallenAll, $mapValue);
+    array_push($lablesAll, ((string) $mapKey) == "" ? "?" : (string) $mapKey);
   }
 
-  // var_dump($valueMap);
-
-  // echo "<br><br>";
-  // var_dump($aantallen);
-  // echo "<br><br>";
-  var_dump($lables);
-  // exit;
-
-  $aantallen2 = array(30, 1567, 3045, 1787, 497, 56, 45); //hexagon
-  $aantallenDiagram2 = array(0, 10, 5, 2, 20, 30, 45);
-  $labelDriagram2 = array( 'January','February','March','April','May','June',);
+  foreach($valueMapMerk as $mapKey => $mapValue) {
+    array_push($aantallenMerk, $mapValue);
+    array_push($lablesMerk, ((string) $mapKey) == "" ? "?" : (string) $mapKey);
+  }
 
 ?>
 <head>
@@ -113,12 +123,11 @@
 
 <script type="text/javascript">
  
-var een_lijstJS2 = <?php echo json_encode($aantallen2) ?>;
-var labelName = <?php echo json_encode($lableName) ?>;
-//labels veranderen naar de opgevraagde data
+var data = <?php echo json_encode($aantallenAll) ?>;
+var labels = <?php echo json_encode($lablesAll) ?>;
 
-var data = <?php echo json_encode($aantallen) ?>;
-var labels = <?php echo json_encode($lables) ?>;
+console.log("data =>", data);
+console.log("lables =>", labels);
 
 const configData = {
   labels,
@@ -163,28 +172,30 @@ window.addEventListener('load', function() {
     </div>
 
 <script type="text/javascript">
-var dataDiagram2 = <?php echo json_encode($aantallenDiagram2) ?>;  
-var labelDiagram2 = <?php echo json_encode($labelDriagram2)?>;
-var labeNaam2 = <?php echo json_encode($labelnaamDia2)?>;
-//labels veranderen naar de opgevraagde data
+const data2 = <?php echo json_encode($aantallenMerk) ?>;
+const labels2 = <?php echo json_encode($lablesMerk) ?>;
 
+console.log("data2 => ", data2);
+console.log("labels2 => ", labels2);
 
-    const labels2 = labelDiagram2;
-
-    //data
-    const data2 = {
-        labels: labels2,
-        datasets: [{
-            label: "???",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: dataDiagram2,
-        }]
+    const configData2 = {
+      labels,
+      datasets: [{
+        label: "todo",
+        data: data2,
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+      }]
     };
 
    //soort grafiek
     const config2 = {
-        type: 'line',
+        type: 'doughnut',
         data: data2,
         options: {}
     };
